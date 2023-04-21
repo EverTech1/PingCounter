d5994e22c164b0b9821e501c6b2d1249c73e6010
EverTech1 <xxtroller13xx@gmail.com>
2023-04-21 18:14:59 +0300

    Fixed issue with button list indexing

--- src/main/java/EverTech/pingcounter/SettingsGui.java
+++ src/main/java/EverTech/pingcounter/SettingsGui.java
@@ -29,22 +29,22 @@ public class SettingsGui extends GuiScreen {
         super.initGui();
         mc.updateDisplay();
         int stringSize = mc.fontRendererObj.getStringWidth("Back");
-        buttonList.add(scalarSlider);
         buttonList.add(new GuiButton(0,(scaledWidth/2)-(stringSize/2)-30, (scaledHeight/2)+70, stringSize+60, 20, "Back"));
         buttonList.add(new GuiButton(1, (scaledWidth/2)-180, (scaledHeight/2)-70, mc.fontRendererObj.getStringWidth("Toggle")+20, 20, "Toggle"));
         buttonList.add(new GuiButton(2, (scaledWidth/2)-180, (scaledHeight/2)-40, mc.fontRendererObj.getStringWidth("Change Position")+20, 20, "Change Position"));
         buttonList.add(new GuiButton(3, (scaledWidth/2)-180, (scaledHeight/2)-10,mc.fontRendererObj.getStringWidth("Color Settings")+20, 20, "Color Settings"));
         buttonList.add(new GuiButton(4, (scaledWidth/2)+30, (scaledHeight/2)-40, mc.fontRendererObj.getStringWidth("Edit Position")+20, 20, "Edit Position"));
+        buttonList.add(scalarSlider);
     }
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
         drawRect((scaledWidth/2)-200,(scaledHeight/2)-100, (scaledWidth/2)+200, (scaledHeight/2)+100, 0xA0000000);
         buttonList.get(4).visible = Main.selection == 9;
         drawString(mc.fontRendererObj, "Ping Counter settings", (scaledWidth/2)-190, (scaledHeight/2)-90, 0xFFFFFF);
-        super.drawScreen(mouseX, mouseY, partialTicks);
         String toWrite = Main.enableDisplay ? "Enabled" : "Disabled";
         drawString(mc.fontRendererObj, toWrite, (scaledWidth/2)-140+mc.fontRendererObj.getStringWidth("Toggle"), (scaledHeight/2)-65, 0xFFFFFF);
         drawString(mc.fontRendererObj, strings[Main.selection][0], (scaledWidth/2)-140+mc.fontRendererObj.getStringWidth("Change Position"), (scaledHeight/2)-35, 0xFFFFFF);
+        super.drawScreen(mouseX, mouseY, partialTicks);
     }
 
     @Override
