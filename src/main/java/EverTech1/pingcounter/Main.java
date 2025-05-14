cf1c3101cb73b1903b2261b248b994cf49afc700
EverTech1 <evertech1mc@gmail.com>
2025-05-15 00:45:38 +0300

    Also don't know

--- src/main/java/EverTech1/pingcounter/Main.java
+++ src/main/java/EverTech1/pingcounter/Main.java
@@ -1,7 +1,6 @@
 package EverTech1.pingcounter;
 
 import com.mojang.blaze3d.platform.InputConstants;
-import com.mojang.logging.LogUtils;
 import net.minecraft.client.KeyMapping;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -11,14 +10,13 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.lwjgl.glfw.GLFW;
-import org.slf4j.Logger;
 
 @Mod(Main.MODID)
 public class Main
 {
+    public static boolean notified = false;
     public static final String MODID = "pingcounter";
-    private static final Logger LOGGER = LogUtils.getLogger();
-    public static KeyMapping keyMap = new KeyMapping("key.pingcounter.open_settings", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_MINUS, "key.categories.misc");
+    public static KeyMapping keyMap = new KeyMapping("key.pingcounter.open_settings", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_MINUS, "key.categories.pingcounter");
     public static Pinger pinger;
     public Main()
     {
