3a023c0327473b4f3f737fec984386806073a77f
EverTech1 <evertech1mc@gmail.com>
2025-05-14 03:06:59 +0300

    Changed default value

--- src/main/java/EverTech1/pingcounter/Config.java
+++ src/main/java/EverTech1/pingcounter/Config.java
@@ -20,8 +20,8 @@ public class Config
     private static final ForgeConfigSpec.IntValue backgroundColorBlueConfig = BUILDER.defineInRange("backgroundColorRed", 0, 0, 255);
     private static final ForgeConfigSpec.BooleanValue textShadowConfig = BUILDER.define("textShadow", true);
     private static final ForgeConfigSpec.DoubleValue scaleConfig = BUILDER.defineInRange("scale", 1.0, 0.0, 10.0);
-    private static final ForgeConfigSpec.DoubleValue posXConfig = BUILDER.defineInRange("posX", 0.1, 0.0, 1.0);
-    private static final ForgeConfigSpec.DoubleValue posYConfig = BUILDER.defineInRange("posY", 0.1, 0.0, 1.0);
+    private static final ForgeConfigSpec.DoubleValue posXConfig = BUILDER.defineInRange("posX", 0.028, 0.0, 1.0);
+    private static final ForgeConfigSpec.DoubleValue posYConfig = BUILDER.defineInRange("posY", 0.05, 0.0, 1.0);
     private static final ForgeConfigSpec.ConfigValue<String> displayTextConfig = BUILDER.define("displayText", "Ping %1$dms");
 
     static final ForgeConfigSpec SPEC = BUILDER.build();
@@ -61,6 +61,21 @@ public class Config
         posX = posXConfig.get();
         posY = posYConfig.get();
         displayText = displayTextConfig.get();
+    }
 
+    static void updateConfig(){
+        enabledConfig.set(enabled);
+        textColorRedConfig.set(textColorRed);
+        textColorGreenConfig.set(textColorGreen);
+        textColorBlueConfig.set(textColorBlue);
+        backgroundColorAlphaConfig.set(backgroundColorAlpha);
+        backgroundColorRedConfig.set(backgroundColorRed);
+        backgroundColorGreenConfig.set(backgroundColorGreen);
+        backgroundColorBlueConfig.set(backgroundColorBlue);
+        textShadowConfig.set(textShadow);
+        scaleConfig.set(scale);
+        posXConfig.set(posX);
+        posYConfig.set(posY);
+        displayTextConfig.set(displayText);
     }
 }
