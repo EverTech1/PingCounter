e24fe43da85842878102a8243e3f1f7cd2109dd4
EverTech1 <evertech1mc@gmail.com>
2025-05-16 01:14:41 +0300

    Reduced pinging interval to 2s

--- src/main/java/EverTech1/pingcounter/Events.java
+++ src/main/java/EverTech1/pingcounter/Events.java
@@ -27,7 +27,7 @@ public class Events {
     }
     @SubscribeEvent
     public static void onJoinServer(ClientPlayerNetworkEvent.LoggingIn event){
-        Main.pinger.startPinging(5000);
+        Main.pinger.startPinging(2000);
         if(!Main.notified){
             ModList.get().getModContainerById(Main.MODID).ifPresent(modContainer -> {
                 VersionChecker.CheckResult res = VersionChecker.getResult(modContainer.getModInfo());
