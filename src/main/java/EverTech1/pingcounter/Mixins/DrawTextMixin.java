f8dce2942b0e7199f9c29abf2cf37def19d5edb0
EverTech1 <evertech1mc@gmail.com>
2025-05-16 02:15:48 +0300

    Stuff

--- src/main/java/EverTech1/pingcounter/Mixins/DrawTextMixin.java
+++ src/main/java/EverTech1/pingcounter/Mixins/DrawTextMixin.java
@@ -2,11 +2,14 @@ package EverTech1.pingcounter.Mixins;
 
 import EverTech1.pingcounter.Config;
 import EverTech1.pingcounter.Pinger;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.*;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.renderer.GameRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -28,7 +31,21 @@ public class DrawTextMixin {
             final String measureString = String.format(Config.displayText, 999);
             final int stringSize = font.width(measureString);
             final double scale = 3*Config.scale/mc.getWindow().getGuiScale();
-            guiGraphics.fill(pos[0]-(int)(5*scale), pos[1]-(int)(5*scale), pos[0]+(int)((stringSize+5)*scale), pos[1]+(int)((font.lineHeight+4)*scale), backgroundColor);
+
+            RenderSystem.enableBlend();
+            RenderSystem.setShader(GameRenderer::getPositionColorShader);
+            RenderSystem.setShaderColor((float) Config.backgroundColorRed /255, (float) Config.backgroundColorGreen /255, (float) Config.backgroundColorBlue /255, (float) Config.backgroundColorAlpha /255);
+            Tesselator tesselator = Tesselator.getInstance();
+            BufferBuilder buffer = tesselator.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_COLOR);
+            buffer.addVertex((float)(pos[0]-(5*scale)), (float)(pos[1]-(5*scale)), 0.0f).setColor(backgroundColor);
+            buffer.addVertex((float)(pos[0]-(5*scale)), (float)(pos[1]+(font.lineHeight+4)*scale), 0.0f).setColor(backgroundColor);
+            buffer.addVertex((float)(pos[0]+(stringSize+5)*scale), (float)(pos[1]+(font.lineHeight+4)*scale), 0.0f).setColor(backgroundColor);
+            buffer.addVertex((float)(pos[0] +(stringSize+5)*scale), (float)(pos[1]-(5*scale)), 0.0f).setColor(backgroundColor);
+            BufferUploader.drawWithShader(buffer.build());
+            RenderSystem.setShaderColor(1,1,1,1);
+            RenderSystem.disableBlend();
+
+//            guiGraphics.fill(pos[0]-(int)(5*scale), pos[1]-(int)(5*scale), pos[0]+(int)((stringSize+5)*scale), pos[1]+(int)((font.lineHeight+4)*scale), backgroundColor);
             guiGraphics.pose().scale((float)scale, (float)scale, 1);
             guiGraphics.drawString(font, String.format(displayString, Pinger.latency), (float)(pos[0]/scale), (float)(pos[1]/scale), textColor, Config.textShadow);
         }
