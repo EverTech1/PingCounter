1596bdd7c75d721a96687790e79739aa6253ca4c
EverTech1 <EverTech1mc@gmail.com>
2025-07-03 01:54:34 +0300

    Open settings with command

--- src/main/java/evertech1/pingcounter/PingCounter.java
+++ src/main/java/evertech1/pingcounter/PingCounter.java
@@ -1,8 +1,12 @@
 package evertech1.pingcounter;
 
 import evertech1.pingcounter.GUI.SettingsGui;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
@@ -14,14 +18,13 @@ import net.minecraft.client.util.InputUtil;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.lwjgl.glfw.GLFW;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+@Environment(EnvType.CLIENT)
 public class PingCounter implements ModInitializer {
 	public static final String MOD_ID = "pingcounter";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	private static final Identifier EXAMPLE_LAYER = Identifier.of(PingCounter.MOD_ID, "ping-display-layer");
 	private static KeyBinding keybind;
+	private static boolean shouldOpenGUI = false;
 	public static boolean isEditing = false;
 	@Override
 	public void onInitialize() {
@@ -33,7 +36,7 @@ public class PingCounter implements ModInitializer {
 		));
 		ConfigHandler.getValues();
 		Pinger.startPinger(1000);
-		HudLayerRegistrationCallback.EVENT.register(layeredDrawer->layeredDrawer.attachLayerAfter(IdentifiedLayer.CHAT, EXAMPLE_LAYER, RenderPing::render));
+		HudLayerRegistrationCallback.EVENT.register(layeredDrawer->layeredDrawer.attachLayerBefore(IdentifiedLayer.CHAT, EXAMPLE_LAYER, RenderPing::render));
 
 		ClientPlayConnectionEvents.JOIN.register((handler, sender, client)->{
 			Pinger.sender = sender;
@@ -46,9 +49,20 @@ public class PingCounter implements ModInitializer {
 		});
 
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
-			while (keybind.wasPressed()) {
-				MinecraftClient.getInstance().setScreen(new SettingsGui(Text.empty()));
+			while (keybind.wasPressed() || shouldOpenGUI) {
+				MinecraftClient.getInstance().execute(()->{
+					shouldOpenGUI = false;
+					MinecraftClient.getInstance().setScreen(new SettingsGui(Text.empty()));
+				});
 			}
 		});
+
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(ClientCommandManager.literal("pingcounter")
+			.executes(context -> {
+				shouldOpenGUI = true;
+				return 1;
+			}))
+		);
+
 	}
 }
