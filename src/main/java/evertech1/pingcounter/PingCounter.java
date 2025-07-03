7a0a75af692ff934ff1d85fac2bd35713dc7caa1
EverTech1 <EverTech1mc@gmail.com>
2025-07-03 19:55:40 +0300

    Version check

--- src/main/java/evertech1/pingcounter/PingCounter.java
+++ src/main/java/evertech1/pingcounter/PingCounter.java
@@ -15,19 +15,31 @@ import net.fabricmc.fabric.api.client.rendering.v1.IdentifiedLayer;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.lwjgl.glfw.GLFW;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URI;
 
 @Environment(EnvType.CLIENT)
 public class PingCounter implements ModInitializer {
+	public static String MOD_VERSION = "1.0.1";
 	public static final String MOD_ID = "pingcounter";
 	private static final Identifier EXAMPLE_LAYER = Identifier.of(PingCounter.MOD_ID, "ping-display-layer");
 	private static KeyBinding keybind;
 	private static boolean shouldOpenGUI = false;
 	public static boolean isEditing = false;
+	private static boolean notified = false;
+	private static final String downloadLink = "PLACEHOLDER_DOWNLOAD";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	@Override
 	public void onInitialize() {
+		VersionChecker.startCheck();
+
 		keybind = KeyBindingHelper.registerKeyBinding(new KeyBinding(
 				"key.pingcounter.open_settings",
 				InputUtil.Type.KEYSYM,
@@ -39,8 +51,16 @@ public class PingCounter implements ModInitializer {
 		HudLayerRegistrationCallback.EVENT.register(layeredDrawer->layeredDrawer.attachLayerBefore(IdentifiedLayer.CHAT, EXAMPLE_LAYER, RenderPing::render));
 
 		ClientPlayConnectionEvents.JOIN.register((handler, sender, client)->{
-			Pinger.sender = sender;
-			Pinger.isConnected = true;
+			if(!client.isIntegratedServerRunning()){
+				Pinger.sender = sender;
+				Pinger.isConnected = true;
+				if(VersionChecker.result == VersionChecker.Results.OLD && !notified){
+					notified = true;
+					if(client.player != null){
+						client.player.sendMessage(Text.literal("Ping Counter mod is outdated. Download latest version ").append(Text.literal("here").setStyle(Style.EMPTY.withColor(5636095).withUnderline(true).withClickEvent(new ClickEvent.OpenUrl(URI.create(downloadLink))))), false);
+					}
+				}
+			}
 		});
 
 		ClientPlayConnectionEvents.DISCONNECT.register((handler, client)->{
@@ -56,7 +76,6 @@ public class PingCounter implements ModInitializer {
 				});
 			}
 		});
-
 		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(ClientCommandManager.literal("pingcounter")
 			.executes(context -> {
 				shouldOpenGUI = true;
