31f2845e64026d14e04e3d81ea67f123bdee8d2b
vagss <evertech1mc@gmail.com>
2025-07-08 15:03:25 +0300

    Scale gui if it doesn't fit on screen

--- src/main/java/EverTech1/pingcounter/SettingsGui.java
+++ src/main/java/EverTech1/pingcounter/SettingsGui.java
@@ -21,6 +21,7 @@ public class SettingsGui extends Screen {
     private Button toggleButton;
     private EditBox editBox;
     private ExtendedSlider scaleSlider;
+    private float scaleFactor = 1;
     protected SettingsGui(Component pTitle) {
         super(pTitle);
     }
@@ -29,32 +30,37 @@ public class SettingsGui extends Screen {
     protected void init() {
         super.init();
         minecraft = Minecraft.getInstance();
-        boxCornerX = (width/2)-(boxWidth/2);
-        boxCornerY = (height/2)-(boxHeight/2);
+        scaleFactor = Math.min(1.0f, Math.min((float)width/boxWidth, (float)height/boxHeight));
+        boxCornerX = (int) ((width/2.0)-(boxWidth*scaleFactor/2.0));
+        boxCornerY = (int) ((height/2.0)-(boxHeight*scaleFactor/2.0));
         final int backStringLength = font.width("Back");
-        toggleButton  = new Button.Builder(Component.literal(Config.enabled?"Enabled":"Disabled"), onPressButton(1)).pos(boxCornerX+20, boxCornerY+30).size(font.width("Disabled")+20, 20).build();
-        addRenderableWidget(new Button.Builder(Component.literal("Back"), onPressButton(0)).pos((width / 2) - (backStringLength / 2) - 30, boxCornerY + boxHeight - 30).size(backStringLength + 60, 20).build());
+        toggleButton  = new Button.Builder(Component.literal(Config.enabled?"Enabled":"Disabled"), onPressButton(1)).pos(20, 30).size(font.width("Disabled")+20, 20).build();
+        addRenderableWidget(new Button.Builder(Component.literal("Back"), onPressButton(0)).pos((boxWidth / 2) - (backStringLength / 2) - 30, boxHeight - 30).size(backStringLength + 60, 20).build());
         addRenderableWidget(toggleButton);
-        addRenderableWidget(new Button.Builder(Component.literal("Color Settings"), onPressButton(2)).pos(boxCornerX+20, boxCornerY + 60).size(font.width("Color Settings") + 20, 20).build());
-        addRenderableWidget(new Button.Builder(Component.literal("Edit Position"), onPressButton(3)).pos(boxCornerX+20, boxCornerY + 90).size(font.width("Edit Position") + 20, 20).build());
-        addRenderableWidget(new Button.Builder(Component.literal("Reset"), onPressButton(4)).pos(boxCornerX+290, boxCornerY + 150).size(font.width("Reset")+60, 20).build());
+        addRenderableWidget(new Button.Builder(Component.literal("Color Settings"), onPressButton(2)).pos(20, 60).size(font.width("Color Settings") + 20, 20).build());
+        addRenderableWidget(new Button.Builder(Component.literal("Edit Position"), onPressButton(3)).pos(20, 90).size(font.width("Edit Position") + 20, 20).build());
+        addRenderableWidget(new Button.Builder(Component.literal("Reset"), onPressButton(4)).pos(290, 150).size(font.width("Reset")+60, 20).build());
 
-        scaleSlider = new ExtendedSlider(boxCornerX+20, boxCornerY+120, 256, 20, Component.literal("Scale: "), Component.literal(""), 0.1, 5.0, Config.scale, 0.05, 0, true);
+        scaleSlider = new ExtendedSlider(20, 120, 256, 20, Component.literal("Scale: "), Component.literal(""), 0.1, 5.0, Config.scale, 0.05, 0, true);
         addRenderableWidget(scaleSlider);
 
         editBox = new EditBox(font, 256, 20, Component.literal("Enter display text here"));
         editBox.setValue(Config.displayText.replaceAll("(%%%%)", "%").replaceAll("(%1\\$d)", Matcher.quoteReplacement("$[ping]")));
-        editBox.setPosition(boxCornerX+20, boxCornerY+150);
+        editBox.setPosition(20, 150);
         addRenderableWidget(editBox);
     }
 
     @Override
-    public void render(GuiGraphics pGuiGraphics, int pMouseX, int pMouseY, float pPartialTick) {
-        pGuiGraphics.fill(boxCornerX, boxCornerY, boxCornerX+boxWidth, boxCornerY+boxHeight, 0xA0000000);
+    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float pPartialTick) {
+        guiGraphics.pose().pushPose();
+        guiGraphics.pose().translate(boxCornerX, boxCornerY, 0);
+        guiGraphics.pose().scale(scaleFactor, scaleFactor, 0);
+        guiGraphics.fill(0, 0, boxWidth, boxHeight, 0xA0000000);
         for(Renderable renderable : this.renderables){
-            renderable.render(pGuiGraphics, pMouseX, pMouseY, pPartialTick);
+            renderable.render(guiGraphics, (int)((mouseX-boxCornerX)/scaleFactor), (int)((mouseY-boxCornerY)/scaleFactor), pPartialTick);
         }
-        pGuiGraphics.drawString(font, "Ping Counter settings", boxCornerX+20, boxCornerY+10, 0xFFFFFFFF);
+        guiGraphics.drawString(font, "Ping Counter settings", 20, 10, 0xFFFFFFFF);
+        guiGraphics.pose().popPose();
     }
 
     private Button.OnPress onPressButton(int id){
@@ -92,5 +98,20 @@ public class SettingsGui extends Screen {
         Config.scale = scaleSlider.getValue();
         Config.displayText = editBox.getValue().replaceAll("%", "%%%%").replaceAll("(\\$\\[ping])", Matcher.quoteReplacement("%1$d"));
         super.tick();
+    }
+
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int button) {
+        return super.mouseClicked((int)((mouseX-boxCornerX)/scaleFactor), (int)((mouseY-boxCornerY)/scaleFactor), button);
+    }
+
+    @Override
+    public boolean mouseDragged(double mouseX, double mouseY, int button, double deltaX, double deltaY) {
+        return super.mouseDragged((int)((mouseX-boxCornerX)/scaleFactor), (int)((mouseY-boxCornerY)/scaleFactor), button, deltaX, deltaY);
+    }
+
+    @Override
+    public boolean mouseReleased(double mouseX, double mouseY, int button) {
+        return super.mouseReleased((int)((mouseX-boxCornerX)/scaleFactor), (int)((mouseY-boxCornerY)/scaleFactor), button);
     }
 }
