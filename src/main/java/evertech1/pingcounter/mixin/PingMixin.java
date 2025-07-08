2a3b9eaea86b4b58c0af090b088cedf843b14ca1
EverTech1 <EverTech1mc@gmail.com>
2025-07-09 01:49:09 +0300

    Fixed getting kicked when server reconfigures

--- src/main/java/evertech1/pingcounter/mixin/PingMixin.java
+++ src/main/java/evertech1/pingcounter/mixin/PingMixin.java
@@ -2,6 +2,7 @@ package evertech1.pingcounter.mixin;
 
 import evertech1.pingcounter.Pinger;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.network.packet.s2c.play.EnterReconfigurationS2CPacket;
 import net.minecraft.network.packet.s2c.query.PingResultS2CPacket;
 import net.minecraft.util.Util;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,5 +15,10 @@ public class PingMixin {
     @Inject(at = @At("HEAD"), method="onPingResult")
     public void onPingResult(PingResultS2CPacket packet, CallbackInfo ci){
         Pinger.ping = Util.getMeasuringTimeMs() - packet.startTime();
+    }
+    @Inject(at = @At("HEAD"), method="onEnterReconfiguration")
+    public void onReconfigure(EnterReconfigurationS2CPacket packet, CallbackInfo ci){
+        Pinger.stopPinger();
+        Pinger.sender = null;
     }
 }
