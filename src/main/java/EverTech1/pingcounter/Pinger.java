2627fa38b8e8944d13bf65aa4f39f9d27431d4be
vagss <evertech1mc@gmail.com>
2025-10-11 00:39:44 +0300

    Fixed potential issue with timers

--- src/main/java/EverTech1/pingcounter/Pinger.java
+++ src/main/java/EverTech1/pingcounter/Pinger.java
@@ -1,38 +1,39 @@
 package EverTech1.pingcounter;
 
 import net.minecraft.Util;
-import net.minecraft.client.Minecraft;
 import net.minecraft.network.Connection;
 import net.minecraft.network.protocol.ping.ServerboundPingRequestPacket;
 
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.concurrent.Executors.newSingleThreadScheduledExecutor;
 
 public class Pinger {
-    public static long latency;
     public static boolean isPinging = false;
-    private static Timer timer;
+    public static long latency = 0;
     public static Connection connection;
-    Minecraft mc;
-    public Pinger(){
-        mc = Minecraft.getInstance();
-    }
-    public void startPinging(int interval){
-        if(!mc.isLocalServer()&&!isPinging) {
-            timer = new Timer();
-            timer.schedule(new TimerTask() {
-                @Override
-                public void run() {
-                    if(connection!=null) connection.send(new ServerboundPingRequestPacket(Util.getMillis()));
-                }
-            }, 3000, interval);
+    private static final Runnable pingerTask = new Runnable() {
+        public void run() {
+            if(connection != null){
+                if(isPinging) connection.send(new ServerboundPingRequestPacket(Util.getMillis()));
+            }
+        }
+    };
+    private final static ScheduledExecutorService pingerExecutor = newSingleThreadScheduledExecutor();
+    private static ScheduledFuture<?> pinger;
+    public static void startPinging(int interval){
+        if(!isPinging) {
             isPinging = true;
+            if(pinger != null && pinger.state() == Future.State.RUNNING) pinger.cancel(true);
+            pinger = pingerExecutor.scheduleAtFixedRate(pingerTask, 3000, interval, TimeUnit.MILLISECONDS);
         }
+
     }
-    public void stopPinging(){
-        if(isPinging){
-            timer.cancel();
-            isPinging = false;
-        }
+    public static void stopPinging(){
+        if(isPinging && pinger.state().equals(Future.State.RUNNING)) pinger.cancel(true);
+        isPinging = false;
     }
 }
